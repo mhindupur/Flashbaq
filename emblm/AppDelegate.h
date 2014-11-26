06488f42317f7645c60e1171e126282a3ed3a2a4
kavyavl <kavyavalavala@ymail.com>
2014-11-26 12:43:11 +0530

    Initial Commit

---
+++ emblm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  emblm
+//
+//  Created by Kavya Valavala on 11/26/14.
+//  Copyright (c) 2014 Vaayoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
